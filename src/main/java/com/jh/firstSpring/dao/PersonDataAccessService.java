8cb9aec14a3feec64af47d49b89494a3e7d5bdcc
hoon-e <32868349+hoon-e@users.noreply.github.com>
2020-06-05 18:02:05 +0900

    finish

---
+++ src/main/java/com/jh/firstSpring/dao/PersonDataAccessService.java
@@ -0,0 +1,36 @@
+package com.jh.firstSpring.dao;
+
+import com.jh.firstSpring.model.Person;
+import org.springframework.stereotype.Repository;
+
+import java.io.*;
+import java.util.*;
+
+@Repository("postgres")
+public class PersonDataAccessService implements PersonDao{
+
+    @Override
+    public int insertPerson(UUID id, Person person) {
+        return 0;
+    }
+
+    @Override
+    public List<Person> selectAllPeople() {
+        return null;
+    }
+
+    @Override
+    public Optional<Person> selectPersonById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deletePersonById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updatePersonById(UUID id, Person person) {
+        return 0;
+    }
+}
